6ce7bd7ba9078b1792ebc915f30ec73dfffb46f8
Calib Serrano <sca@outlook.ph>
2023-12-13 22:16:08 +0800

    change database credentials

--- serrano_calib/api/db_conn.php
+++ serrano_calib/api/db_conn.php
@@ -1,7 +1,14 @@
 <?php
-  $host = "localhost";
-  $username = "calib";
-  $db = "movie";
-  $conn = mysqli_connect ($host, $username, $password, $db);
-  if (!$conn) return;
+/*
+$host = "localhost";
+$username = "calib";
+$db = "movie";
+*/
+
+$host = "127.0.0.1:3306";
+$username = "u754510873_user_exercise";
+$password = "zK9]bR|Sgx/U";
+$dbname = "u754510873_db_exercise18";
+$conn = mysqli_connect ($host, $username, $password, $db);
+if (!$conn) return;
 ?>
